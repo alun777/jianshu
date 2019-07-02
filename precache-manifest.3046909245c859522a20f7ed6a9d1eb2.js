895487f2698ac6d4c1a59a80fee866ffceea8871
ChaoYIN <yinchao428@hotmail.com>
2019-07-02 22:40:11 +1000

    Updates

---
+++ precache-manifest.3046909245c859522a20f7ed6a9d1eb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066c2e6b6ed81e414f120824fdca1c9c",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "438b6082a099169504db",
+    "url": "/jianshu/static/js/2.1f93887f.chunk.js"
+  },
+  {
+    "revision": "fbbea626472b47758d5b",
+    "url": "/jianshu/static/js/3.a124dbc8.chunk.js"
+  },
+  {
+    "revision": "1e2b070422bb2e748bc4",
+    "url": "/jianshu/static/js/main.701214aa.chunk.js"
+  },
+  {
+    "revision": "f68f189ca5aed3a98ec5",
+    "url": "/jianshu/static/js/runtime~main.8b30babc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d106177f3c837ad7aa77489d12ab93a.js"
+  "/jianshu/precache-manifest.3046909245c859522a20f7ed6a9d1eb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jianshu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
